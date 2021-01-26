2ee45475d35977b51e3dfe3bd51d470605e35a9a
Reivaxboido <xavierteor@gmail.com>
2021-01-26 16:31:44 +0800

    Add Console.log for debugging

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function characterroll() {
     var roll = Math.floor(Math.random() * 1000);
-    console.log(roll);
     var star = 0;
     if (roll < 6) {
         star = 5;
@@ -9,12 +8,13 @@ function characterroll() {
     } else {
         star = 3;
     }
+    console.log(roll);
+    console.log(star);
     return star;
 }
 
 function weaponroll() {
     var roll= Math.floor(Math.random() * 1000);
-    console.log(roll);
     var star = 0;
     if (roll < 7) {
         star = 5;
@@ -23,5 +23,7 @@ function weaponroll() {
     } else {
         star = 3;
     }
+    console.log(roll);
+    console.log(star);
     return star;
 }
